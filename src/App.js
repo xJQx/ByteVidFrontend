884acc934d11adb5303852642a3d6fc6a14fd0ad
Jing Hua <tohjinghua123@gmail.com>
2022-10-01 21:26:57 +0800

    merge changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Header from './components/Header/Header' 
+import Header from './components/Header/Header';
 import InputTabs from './components/InputTabs/InputTabs';
 import ThemeMode from './components/ThemeMode/ThemeMode';
+import ResultTabs from './components/ResultTabs/ResultTabs';
 
 function App() {
   return (
-    <div className="lg:text-center bg-stone-100 dark:bg-slate-800">
+    <div className='lg:text-center bg-stone-100 dark:bg-slate-800'>
       <ThemeMode />
       <Header />
+      <ResultTabs />
       <InputTabs />
     </div>
   );
